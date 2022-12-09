b6f811f96b6c359a98cccfb7183682beaed69992
yuxiny1 <yuxiny1@me.com>
2022-12-09 00:37:12 +0000

    update
    update

---
+++ creat_3.sql
@@ -0,0 +1,9 @@
+show tables;
+
+describe foods;
+
+INSERT INTO foods (name, Typical_values_per, Unit_of_the_typical_value, Carbs_per, Unit_of_the_carbs, Fat_per, Unit_of_the_fat, Protein_per, Unit_of_the_protein) VALUES ('Apple', 0.5, 'piece', 0.12, 'g', 0.17, 'g', 0.26, 'g');
+
+INSERT INTO foods (name, Typical_values_per, Unit_of_the_typical_value, Carbs_per, Unit_of_the_carbs, Fat_per, Unit_of_the_fat, Protein_per, Unit_of_the_protein) VALUES ('Banana', 1, 'piece', 0.23, 'g', 0.12, 'g', 0.33, 'g');
+
+select* from foods;
